217b39fde33da4eef2e0058f67835d640d361a2b
pepapig20k <roberto.poblete14@gmail.com>
2022-12-28 23:48:35 -0300

    ejercicio3

--- hola.py
+++ hola.py
@@ -1,3 +1,14 @@
-h = "Hola Mundo!"
-h = "Hellow Word"
-print(h)
+# primera letra de cada oracion en mayuscula se usa = variable.title()
+# primera letra como mayuscula y todo en minuscula es = variable.capitalixe()
+# todo en minuscula = variable.lower()
+# todo en mayuscula = variable.upper()
+# para reemplazar letras en especifico = varibale.replace('letra', 'letra a reemplazar')
+# para buscar palabras = variable.find('texto')
+# convertir una cadena en una lista = variable.split()
+# pasar de lista a cadena = ''.join(nombre de lista)
+# 
+#
+#
+#
+#
+#
