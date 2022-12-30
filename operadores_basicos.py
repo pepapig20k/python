eee8db0cfe1745d0a8cba72c200f24e057dfd2c9
pepapig20k <roberto.poblete14@gmail.com>
2022-12-30 00:28:27 -0300

    python

---
+++ operadores_basicos.py
@@ -0,0 +1,31 @@
+# > mayor que
+# >= mayor o igual 
+# == exactamente igual
+# <= menor o igual 
+# < menor que
+######################################
+# AND -> True, False                 # AND solo da True cuando los dos son True
+# True and True = True               #
+# True and False = False             #
+# False and True = False             #
+# False and False = False            #
+######################################
+# OR -> True, False                  # OR solo da True cuando unos de ellos es True
+# True  or True = True               #
+# True  or False = True              #
+# False or True = True               #
+# False or False = False             #
+######################################
+# while -> mientras la condicion sea True: ejecuta las acciones
+# y deja de ejecutarlas al momento de que sea False
+###############################################################
+# FOR variable in range():
+#
+#
+#
+#
+#
+#
+#
+#
+#
