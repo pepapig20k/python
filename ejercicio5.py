f3efe172a22d8a0568e5b30de5fb9ebe94c2f4d5
pepapig20k <roberto.poblete14@gmail.com>
2022-12-30 23:10:04 -0300

    python

--- ejercicio5.py
+++ ejercicio5.py
@@ -6,3 +6,11 @@ def bisiesto(año):
         print(f"El año {año} no es biciesto")
 
 bisiesto(2151)
+print("")
+bisiesto(1992)
+print("")
+bisiesto(1889)
+print("")
+bisiesto(1988)
+print("")
+bisiesto(1996)
